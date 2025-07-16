811de356bf12f4d0fc998fdfe393f72656c16ec4
z00909923 <zhouchengxu2@huawei.com>
2025-07-16 16:52:42 +0800

    add OIDC RegisterClient resource

--- huaweicloud/provider.go
+++ huaweicloud/provider.go
@@ -2267,6 +2267,7 @@ func Provider() *schema.Provider {
 			"huaweicloud_identitycenter_access_control_attribute_configuration": identitycenter.ResourceAccessControlAttributeConfiguration(),
 			"huaweicloud_identitycenter_provision_permission_set":               identitycenter.ResourceProvisionPermissionSet(),
 			"huaweicloud_identitycenter_scim_user":                              identitycenter.ResourceIdentityCenterSCIMUser(),
+			"huaweicloud_identitycenter_client":                                 identitycenter.ResourceIdentityCenterClient(),
 
 			"huaweicloud_iec_eip":                 iec.ResourceEip(),
 			"huaweicloud_iec_keypair":             iec.ResourceKeypair(),
